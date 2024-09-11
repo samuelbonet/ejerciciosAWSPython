635d2f63a1b9c1ef20e47d67562cc3599e13bdb8
Samuel Bonet <95564083+samuelbonet@users.noreply.github.com>
2024-09-11 17:16:08 +0000

    update funciones

--- environment/flowControl.py
+++ environment/flowControl.py
@@ -1,1 +1,25 @@
+'''for
+for i in "Samuel":
+    print(i)'''
 
+'''while'''
+i = 0
+while i < 6:
+  print(i)
+  i += 2
+
+
+'''listas
+alumnos= ["Carlos","Marc","Alba"]
+print(alumnos[2])'''
+
+
+'''bucles y listas
+for jugadores in ["messi","cristiano","iniesta"]:
+  print(jugadores)'''
+
+'''diccionarios
+alumnos={"Nombre":"Carlos",
+         "Apellido":"Dolz",
+         "Aprende":"AWS"}
+print(alumnos.get("Nombre"))'''
---
+++ funciones.py
@@ -0,0 +1,18 @@
+'''
+def calcularAreaCuadrado():
+    return r**2
+
+r= int(input("Introduce dos lados"))
+
+area= calcularAreaCuadrado(r)
+print(area)'''
+
+
+def calcularAreaCuadrado():
+    return str("hola") + lado1 * lado1
+
+lado1= int(input("Introduce un lado "))
+
+
+area=calcularAreaCuadrado()
+print(area)
